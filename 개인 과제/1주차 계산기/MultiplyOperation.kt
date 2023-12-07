427a90b0085d1439d9ae005e4df8f0d610af5323
lazzzykim <151768821+lazzzykim@users.noreply.github.com>
2023-12-07 16:47:09 +0900

    Add files via upload

---
+++ 개인 과제/1주차 계산기/MultiplyOperation.kt
@@ -0,0 +1,5 @@
+package calculator
+
+class MultiplyOperation {
+    fun operation(num1 : Double, num2 : Double) : Double = num1 * num2
+}
---
+++ 개인 과제/1주차 계산기/SubstractOperation.kt
@@ -0,0 +1,5 @@
+package calculator
+
+class SubstractOperation {
+    fun operation(num1 : Double, num2 : Double) : Double = num1 - num2
+}
